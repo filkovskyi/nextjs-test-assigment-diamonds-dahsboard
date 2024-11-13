8e929797d660a9aed5d896b8d6907e52b99c8fa0
Serhii Filkovskyi <filkovskyi@gmail.com>
2024-11-13 17:51:43 +0200

    remove fonts

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,11 +1,8 @@
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
-import {  geistMono } from '../layout';
 
 export default function DiamondLogo() {
   return (
-    <div
-      className={`${geistMono.className} geistMono flex flex-row items-center leading-none text-white`}
-    >
+    <div className={"flex flex-row items-center leading-none text-white"}>
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Diamond</p>
     </div>
